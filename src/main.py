29bd6096d1445569d970077f3f8a3c778a626d01
Evelyn Pombal Barbosa <evelyn.barbosa@ish.com.br>
2024-06-06 15:37:42 -0300

    Get all epsodes

--- src/main.py
+++ src/main.py
@@ -19,7 +19,24 @@ def get_rick_and_morty_characters():
         return response_json
     
     except requests.exceptions.RequestException as e:
-        logging.info(f"Request failed: {e}")
+        logging.info(f"Error: {e}")
         raise
 
+
+@task(max_retries=3, retry_delay_seconds=5)
+def get_rick_and_morty_episodes():
+    """
+        Retorna todos os epsódios. 
+        Em caso de falha a task será reexecutada por mais 3 vezes com delay de 5s em cada tentativa.
+    """
     
+    try:
+        response = requests.get(f"{URL_API}/episode")
+        response.raise_for_status()
+        response_json = response.json()
+        
+        return response_json
+    
+    except requests.exceptions.RequestException as e:
+        logging.info(f"Error: {e}")
+        raise
